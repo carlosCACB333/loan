6a7aa1e251a75c2c4f705fcb21d92ed58afca3fc
Carlos Castillo Blas <carlosCACB333@gmail.com>
2022-09-20 09:42:25 -0500

    cambiando el directorio de arranque de la app

--- docs/interfaces/contract.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ContractStatuses = exports.OperationTypes = void 0;
-exports.OperationTypes = ['payment', 'loan'];
-exports.ContractStatuses = ['active', 'inactive', 'pending', 'expired', 'deleted'];
